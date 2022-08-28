59ea6c98dc2460627b5b9f887044eae9680d9ae4
spaenleh <spaenleh@gmail.com>
2022-08-28 21:02:47 +0200

    feat: slide show with auto-scroll for figures

--- src/components/ShowFigures.tsx
+++ src/components/ShowFigures.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
+import { useEffect } from 'react';
 
 import { Stack, styled } from '@mui/material';
 
@@ -14,12 +15,24 @@ type Props = {
 };
 
 const ShowFigures: FC<Props> = ({ figures }) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
   const figureWall = figures.map((fSrc, idx) => (
     <ImagePreview key={`fig.${idx}`} alt={`fig.${idx}`} imageSrc={fSrc} />
   ));
 
+  // scroll to end of figure list when new figure is added
+  useEffect(() => {
+    if (containerRef?.current) {
+      // uses "smooth" to get a nice scrolling animation
+      containerRef.current.scrollTo({
+        left: containerRef.current.scrollWidth,
+        behavior: 'smooth',
+      });
+    }
+  }, [figures]);
+
   return (
-    <StyledContainer direction="row" spacing={2}>
+    <StyledContainer ref={containerRef} direction="row" spacing={2}>
       {figureWall}
     </StyledContainer>
   );
